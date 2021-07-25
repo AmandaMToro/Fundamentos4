5b108e32abf5b6b2949ca3b621ef24e7dcb8908c
AmandaMToro <amandamorillo17@gmail.com>
2021-07-25 12:24:42 +0200

    feature(TRIANGULO) class triangulo y calculado el metodo

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 import { Punto } from "./punto";
+import {Triangulo} from "./triangulo"
 
 let coordenadas1:Punto = new Punto(5,3);
 let coordenadas2:Punto = new Punto(9,70);
 let coordenadas3:Punto = new Punto(2,4);
 let coordenadas4:Punto = new Punto(25,30)
 let arrayDePuntos = [coordenadas2,coordenadas3,coordenadas4]
+
+let arraysLados:Triangulo = new Triangulo(coordenadas1, coordenadas2, coordenadas3)
 
 
 console.log("Imprimir Coordenadas");
@@ -17,3 +20,7 @@ console.log("cuadrante");
 console.log(coordenadas3.calcularCuadrante());
 console.log("Calcular los más cercanos");
 console.log(coordenadas1.calcularMasCercano(arrayDePuntos));
+console.log("Calcular longitud lados");
+console.log(arraysLados.calcularLongitudLados());
+
+
